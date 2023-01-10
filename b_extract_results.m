4b71997d1850ef0c8cb98f0b92a4332bc0eec9c2
s-jermy <stephen.jermy@uct.ac.za>
2023-01-10 11:55:07 +0200

    Update b_extract_results.m
    Complete overhaul of results extraction. Everything can be extracted and at the end certain results can be excluded before being written.

--- b_extract_results.m
+++ b_extract_results.m
@@ -1,99 +1,101 @@
-files = {...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_002\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_002\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_004\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_004\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_006\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_006\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_008\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_008\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_009\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_009\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_010\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_010\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_011\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_011\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_012\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_012\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_013\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_013\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_01STEVE_DTI_014\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_01STEVE_DTI_014\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_016\affreg_HRcorr_dti_BH\affreg_HRcorr_dti_BH.xlsx',...
-    'D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers\DTI2_resources\data\STEVE_DTI_016\affreg_HRcorr_dti_CS\affreg_HRcorr_dti_CS.xlsx'...
-    };
-len = length(files);
+main = uigetdir('D:\Steve\OneDrive - University of Cape Town\Documents\PhD\Papers');
+listing = dir(fullfile(main,'**','*.xlsx'));
+len = length(listing);
 fname = ['dti_' datestr(today,'yyyymmdd') '.xlsx'];
 
 T = table;T2 = table;
-count = 0;
 
-lowB = {'b50'};lowB1 = {'b050'};
-highB = {'b450'};
 for i = 1:len
-    count = count+1;
-    for j = 1:3
-        switch j
-            case 1
-                slice = {'Base'};
-                range = 'B6:S12';
-            case 2
-                slice = {'Mid'};
-                range = 'B6:S12';
-            case 3
-                slice = {'Apex'};
-                range = 'B6:S10';
-                
-        end
-        sheet = [lowB{1} '_' highB{1} '_' slice{1} '_Sys'];
-        
-        warning('off','MATLAB:table:ModifiedAndSavedVarnames');
-        try
-            tbl = readtable(files{i},'Sheet',sheet,'Range',range);
-            ID = readcell(files{i},'Sheet',sheet,'Range','B1:B1');
-        catch
-            warning('sheet does not exist');
-            continue
-        end
-        warning('on','MATLAB:table:ModifiedAndSavedVarnames');
-        
-        h = height(tbl);
-        ID2 = repmat(ID,h,1);
-        tech = repmat(count,h,1);
-        slice = repmat(slice,h,1);
-        lowB2 = repmat(lowB1,h,1);
-        highB2 = repmat(highB,h,1);
-        segment = (1:h);
-        tbl2 = table(ID2,tech,slice,lowB2,highB2,segment','VariableNames',{'ID','tech','Slice','lowB','highB','segment'});
-        T = [T;tbl2 tbl];
-        %ID tech Slice lowB highB segment MD std FA std AD std RD std HAd std HAg std absE2A std TRA std SA std
-    end
-    warning('off','MATLAB:table:ModifiedAndSavedVarnames');
-    tbl = readtable(files{i},'Sheet','Summary','Range','B1:V7','VariableNamingRule','modify');
-    warning('on','MATLAB:table:ModifiedAndSavedVarnames');
-    tbl = rmmissing(tbl);
-    tbl = tbl(2:2:end,:);
+    xlfile = fullfile(listing(i).folder,listing(i).name);
+    ds = spreadsheetDatastore(xlfile);
+    sheets = sheetnames(ds,1);
     
-    h = height(tbl);
-    ID2 = repmat(ID,h,1);
-    tech = repmat(count,h,1);    
-    tbl2 = table(ID2,tech,'VariableNames',{'ID','tech'});
-    T2 = [T2;tbl2 tbl];
-    count = mod(count,2);
-    %ID tech Slice lowB highB MD std FA std AD std RD std HAd std HAg std absE2A std TRA std SA std
+    t = split(listing(i).folder,filesep);
+    tok = split(t(end),'_');
+    for j = 1:length(sheets)
+        warning('off','MATLAB:table:ModifiedVarnames');
+        ds.Sheets = j;
+        ds.Range = '';
+        ds.ReadVariableNames = 1;
+        if j>1
+            ds.NumHeaderLines = 5;
+        end
+        tbl = read(ds);
+        tbl = tbl(:,2:end);
+        
+        if j==1
+            ds.Sheets = 2;
+        end
+        ds.Range = 'B1:B1';
+        ds.NumHeaderLines = 0;
+        ds.ReadVariableNames = 0;
+        ID = read(ds);
+        ID = ID{1,1};
+        warning('on','MATLAB:table:ModifiedVarnames');
+        
+        if strcmp(ID,'O3TPR_C00-00_21261')
+            ID = {'O3TPR_CD01_20877'};
+        elseif strcmp(ID,'O3TPR_CD01_1234')
+            ID = {'O3TPR_CD01_20632'};
+        elseif strcmp(ID,'O3TPR_CD01_7777')
+            ID = {'O3TPR_CD01_20769'};
+        end
+
+        h = height(tbl);
+        ID = repmat(ID,h,1);
+        tech = repmat(tok(end),h,1);
+        if j>1
+            tok2 = split(sheets(j),'_');
+            lowB = repmat({tok2{1}},h,1);
+            highB = repmat({tok2{2}},h,1);
+            slice = repmat({tok2{3}},h,1);
+            segment = (1:h);
+            tbl2 = table(ID,tech,slice,lowB,highB,segment','VariableNames',{'ID','tech','Slice','lowB','highB','segment'});
+            T = [T;tbl2 tbl];
+        else
+            tbl2 = table(ID,tech,'VariableNames',{'ID','tech'});
+            T2 = [T2;tbl2 tbl];
+        end
+    end
 end
 
-%% segments
+%%
+lowB = T.lowB;
+lowB2 = T2.lowB;
+ind = find(lowB == "b15");lowB(ind) = repmat({'b015'},size(ind));
+ind = find(lowB == "b50");lowB(ind) = repmat({'b050'},size(ind));
+ind = find(lowB2 == "b15");lowB2(ind) = repmat({'b015'},size(ind));
+ind = find(lowB2 == "b50");lowB2(ind) = repmat({'b050'},size(ind));
+T.lowB = lowB;
+T2.lowB = lowB2;
+
+[~,~,ic] = unique(categorical(T.tech));
+T.tech = ic;
+[~,~,ic] = unique(categorical(T2.tech));
+T2.tech = ic;
+
+%% remove extra entry in segment sheet (average of myocardium)
+exclude = find(ismember(T.segment,7))';
+T(exclude,:) = [];
+
+%% write segments
+exclude = find(ismember(T.lowB,'b350') & ismember(T.highB,'b450'))';
+if isempty(exclude)
+    exclude = find(ismember(T.lowB,'b015') & ismember(T.highB,'b450'))';
+end
+T(exclude,:) = [];
+
 warning('off','MATLAB:xlswrite:AddSheet');
-writetable(T,fname,'Sheet',1);
+writetable(T,fullfile(main,fname),'Sheet',1);
 warning('on','MATLAB:xlswrite:AddSheet');
 
-%% global
-lowB = T2.lowB;
-ind = find(lowB == "b50");
-lowB(ind) = repmat({'b050'},size(ind));
-T2.lowB = lowB;
+%% write global
+exclude = find(ismember(T2.lowB,'b350') & ismember(T2.highB,'b450'))';
+if isempty(exclude)
+    exclude = find(ismember(T2.lowB,'b015') & ismember(T2.highB,'b450'))';
+end
+T2(exclude,:) = [];
 
 warning('off','MATLAB:xlswrite:AddSheet');
-writetable(T2,fname,'Sheet',2);
+writetable(T2,fullfile(main,fname),'Sheet',2);
 warning('on','MATLAB:xlswrite:AddSheet');
