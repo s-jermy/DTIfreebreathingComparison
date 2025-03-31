8d93eeca5dbcd6dee31600d1d2566d46a542ebea
Stephen <stephen.jermy@uct.ac.za>
2025-03-31 10:22:59 +0200

    Update descriptive_statistics.m

--- descriptive_statistics.m
+++ descriptive_statistics.m
@@ -1,6 +1,6 @@
 %%% requires Statistics and Machine Learning Toolbox
 
-function [av,med,quant,skew,kurt] = descriptive_statistics(tbl,type,outl_sd,display_on)
+function [bytech,all] = descriptive_statistics(tbl,type,outl_sd,display_on)
 
 switch type
     case {'MD','AD','RD'}
@@ -36,19 +36,41 @@ if display_on
 end
 
 p = 0:0.25:1;
-quant = quantile(tbl2.(type),p);
-av = mean(tbl2.(type));
-med = median(tbl2.(type));
-skew = skewness(tbl2.(type));
-kurt = kurtosis(tbl2.(type));
+all.quant = quantile(tbl2.(type),p);
+all.av = mean(tbl2.(type));
+all.sd = std(tbl2.(type));
+all.med = median(tbl2.(type));
+all.skew = skewness(tbl2.(type));
+all.kurt = kurtosis(tbl2.(type));
+
+t = unique(tbl2.tech);
+b = unique(tbl2.lowB);
+bytech.quant = zeros([length(t) length(p) length(b)+1]);
+
+for i = 1:length(t)
+    bytech.quant(i,:,1) = quantile(tbl2.(type)(tbl2.tech==t(i)),p);
+    bytech.av(i,1) = mean(tbl2.(type)(tbl2.tech==t(i)));
+    bytech.sd(i,1) = std(tbl2.(type)(tbl2.tech==t(i)));
+    bytech.med(i,1) = median(tbl2.(type)(tbl2.tech==t(i)));
+    bytech.skew(i,1) = skewness(tbl2.(type)(tbl2.tech==t(i)));
+    bytech.kurt(i,1) = kurtosis(tbl2.(type)(tbl2.tech==t(i)));
+    for j = 1:length(b)
+        bytech.quant(i,:,j+1) = quantile(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)),p);
+        bytech.av(i,j+1) = mean(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)));
+        bytech.sd(i,j+1) = std(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)));
+        bytech.med(i,j+1) = median(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)));
+        bytech.skew(i,j+1) = skewness(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)));
+        bytech.kurt(i,j+1) = kurtosis(tbl2.(type)(tbl2.tech==t(i)&tbl2.lowB==b(j)));
+    end
+end
 
 if display_on
     fprintf('Quantiles of %s\n',type);
-    fprintf('%.4f\t',p);fprintf('\n');fprintf('%.4f\t',quant);fprintf('\n');
+    fprintf('%.4f\t',p);fprintf('\n');fprintf('%.4f\t',quant_all);fprintf('\n');
     fprintf('Mean and median of %s\n',type);
-    fprintf('%.4f\t%.4f\n',av,med);
+    fprintf('%.4f\t%.4f\n',av_all,med_all);
     fprintf('Skewness and kurtosis of %s\n',type);
-    fprintf('%.4f\t%.4f\n',skew,kurt);
+    fprintf('%.4f\t%.4f\n',skew_all,kurt_all);
     
     boxplots(tbl,type,outl_sd,0);
 end
