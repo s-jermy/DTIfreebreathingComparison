b3f6abc8d854c49334a9738a2e57e86101f2c020
Stephen <stephen.jermy@uct.ac.za>
2026-07-22 10:46:43 +0200

    Update a_copy_files.m
    Neatened up the copying

--- a_copy_files.m
+++ a_copy_files.m
@@ -3,7 +3,7 @@ if ismac
 else
     main = 'D:\Steve\OneDrive - University of Cape Town\Documents'; %base of main directory
 end
-load1 = fullfile(main,'MATLAB','DTIanalysis'); %base directory to load from
+load1 = fullfile(main,'MATLAB','DTIanalysis','output'); %base directory to load from
 save1 = fullfile(main,'PhD','Papers'); %base directory to save to
 l_rej = 'RejectImages';
 l_bas = 'Systole_Base';
@@ -13,7 +13,7 @@ l_ape = 'Systole_Apex';
 load2 = uigetdir(load1,'Choose folder to load from...');
 temp = split(load2,filesep);
 save2 = uigetdir(save1,'Choose folder to save to...');
-save2 = fullfile(save2,'data');
+save2 = fullfile(save2,'resources','data');
 
 listing = dir(load2);
 listing = listing([listing.isdir]);
@@ -40,9 +40,22 @@ for j = 1:length(listing)
         if strcmp(l_subj,'STEVE_DTI_01STEVE_DTI_014')
             s_subj = 'STEVE_DTI_014';
         end
+    elseif contains(temp{end},'steve_cmo')
+        switch j
+            case 1
+                s_subj = 'PATIENT1';
+            case 2
+                s_subj = 'PATIENT6';
+            case 3
+                s_subj = 'PATIENT5';
+            case 4
+                s_subj = 'PATIENT3';
+            case 5
+                s_subj = 'PATIENT4';
+        end
     end
     
-    listing2 = dir(fullfile(load2,l_subj,'**','Paths.mat'));
+    listing2 = dir(fullfile(load2,l_subj,'*','Paths.mat'));
     
     %% copy spreadsheet of results (and images) out of each methods folder
     for i = 1:length(listing2)
@@ -53,7 +66,7 @@ for j = 1:length(listing)
         l_meth = analysisTag;
         s_meth = temp2{end};
         
-        %mislabeled methods
+        %%mislabeled methods
         % if strcmp(temp{end},'steve_oxford_2021')
         %     if strcmp(l_subj,'O3TPR_C00-00_21261')
         %         s_meth = s_meth(1:end-1); %remove trailing 2 from some method names
@@ -77,31 +90,55 @@ for j = 1:length(listing)
             
             load3 = fullfile(l_temp,l_rej);
             save3 = fullfile(s_fold,l_rej);
-            try
-                copyfile(load3,save3);
-            catch
+            if ~exist(save3,'dir')
+                mkdir(save3);
             end
+            copyimages(load3,save3);
             
             load3 = fullfile(l_temp,l_bas);
             save3 = fullfile(s_fold,l_bas);
-            try
-                copyfile(load3,save3);
-            catch
+            if ~exist(save3,'dir')
+                mkdir(save3);
             end
+            copyimages(load3,save3);
             
             load3 = fullfile(l_temp,l_mid);
             save3 = fullfile(s_fold,l_mid);
-            try
-                copyfile(load3,save3);
-            catch
+            if ~exist(save3,'dir')
+                mkdir(save3);
             end
+            copyimages(load3,save3);
             
             load3 = fullfile(l_temp,l_ape);
             save3 = fullfile(s_fold,l_ape);
-            try
-                copyfile(load3,save3);
-            catch
+            if ~exist(save3,'dir')
+                mkdir(save3);
             end
+            copyimages(load3,save3);
+        end
+    end
+end
+
+function copyimages(load,save)
+    files = dir(fullfile(load,'**'));
+
+    % remove folders
+    files = files(~[files.isdir]);
+    
+    for f_idx = 1:length(files)
+        [~,~,ext] = fileparts(files(f_idx).name);
+        
+        % skip .fig files
+        if strcmpi(ext, '.fig')
+            continue
+        end
+        
+        src = fullfile(files(f_idx).folder, files(f_idx).name);
+        dst = save;
+        
+        try
+            copyfile(src, dst);
+        catch
         end
     end
 end
